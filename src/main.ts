2630777ff399375ba871d51077129da77ee70e34
Phan Van Tuan <phanvantuan99hth@gmail.com>
2021-01-31 15:44:00 +0700

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PORT } from 'config/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  const options = new DocumentBuilder()
+    .setTitle('SPMS API')
+    .setDescription('Study Program Management System API description')
+    .setVersion('1.0')
+    .addTag('SPMS API')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' }, 'token')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/swagger', app, document);
+  await app.listen(PORT);
 }
 bootstrap();
