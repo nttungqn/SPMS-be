98280e414343c4f4e673ede1dba8ba373816fad2
Phan Van Tuan <phanvantuan99hth@gmail.com>
2021-02-08 17:00:31 +0700

    update/add new table roles - update users have roles

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { AppService } from './app.service';
 import { DatabaseConnectionService } from './database-connection/database-connection.service';
 import { ChuongTrinhDaoTaoModule } from './chuong-trinh-dao-tao/chuong-trinh-dao-tao.module';
 import { AuthModule } from './auth/auth.module';
+import { RolesService } from './roles/roles.service';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRootAsync({ useClass: DatabaseConnectionService }), ChuongTrinhDaoTaoModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRootAsync({ useClass: DatabaseConnectionService }),
+    ChuongTrinhDaoTaoModule,
+    AuthModule,
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService, DatabaseConnectionService]
 })
---
+++ src/roles/interfaces/roles.interface.ts
@@ -0,0 +1,7 @@
+export interface IRole {
+  readonly ID: number;
+  readonly name: string;
+  readonly createdAt?: Date;
+  readonly updatedAt?: Date;
+  readonly isDeleted?: boolean;
+}
