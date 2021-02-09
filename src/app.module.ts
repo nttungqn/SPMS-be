1dc640fcf80dcec71f54f028792b27e2aa3b0dd5
Phan Van Tuan <phanvantuan99hth@gmail.com>
2021-02-09 20:30:54 +0700

    update validator email with regex

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,6 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { RolesService } from './roles/roles.service';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { bodyValidatorMiddleware } from 'auth/middlewares/body-validator.middleware';
 
 @Module({
   imports: [
@@ -19,4 +20,8 @@ import { UsersModule } from './users/users.module';
   controllers: [AppController],
   providers: [AppService, DatabaseConnectionService]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(bodyValidatorMiddleware).forRoutes('auth/login', 'auth/signup');
+  }
+}
