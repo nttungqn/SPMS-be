7df8a7118231c112840264b4a05c19599b02321a
Phan Van Tuan <phanvantuan99hth@gmail.com>
2021-03-01 20:15:15 +0700

    update profile

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { USER_MESSAGE } from 'constant/constant';
 import { Repository } from 'typeorm';
 import { UsersEntity } from './entity/user.entity';
 import { IUser } from './interfaces/users.interface';
@@ -20,5 +21,21 @@ export class UsersService {
       .leftJoinAndSelect('users.role', 'roles')
       .where('users.id = :ID and users.email = :email', { ...query })
       .getOne();
+  }
+  async update(ID: number, updateData): Promise<any> {
+    const user = await this.usersRepository.findOne({ ID, isDeleted: false });
+    if (!user) {
+      throw new HttpException(USER_MESSAGE.USER_ID_NOT_FOUND, HttpStatus.BAD_REQUEST);
+    }
+    try {
+      const updated = await this.usersRepository.save({
+        ...user,
+        ...updateData,
+        updatedAt: new Date()
+      });
+      return updated;
+    } catch (error) {
+      throw new HttpException(error?.message || 'error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
