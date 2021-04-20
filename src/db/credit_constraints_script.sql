d8b3a0d23a49e04ca96ba8e9592b8c11829aba12
Nguyen Thanh Tung <nttung99qn@gmail.com>
2021-04-21 00:27:09 +0700

    #patched bug fix

---
+++ src/db/credit_constraints_script.sql
@@ -0,0 +1,103 @@
+use `qHtJYlp09Y`;
+SET SQL_SAFE_UPDATES = 0;
+
+# -------------------------------------------
+DROP TRIGGER IF EXISTS update_tcbb_gn_by_mh;
+delimiter $$
+CREATE TRIGGER update_tcbb_gn_by_mh after update on MonHoc for each row
+begin
+	if old.SoTinChi <> new.SoTinChi and new.isDeleted = false then
+		update GomNhom gn
+		inner join (
+			select ctgn.ID_GomNhom, sum(mh.soTinChi) as TongTC_GN 
+			from ChiTietGomNhom ctgn
+			inner join MonHoc mh ON mh.id = ctgn.ID_MonHoc
+			inner join GomNhom gn on gn.ID = ctgn.ID_GomNhom
+			group by ctgn.ID_GomNhom
+            having ctgn.isDeleted = false and mh.isDeleted = false and gn.isDeleted = false) nt1 
+		on gn.ID = nt1.ID_GomNhom
+		set gn.SoTCBB = nt1.TongTC_GN
+        where gn.LoaiNhom = "BB" and gn.isDeleted = false;
+	end if;
+end
+$$
+delimiter ;
+
+# -------------------------------------------
+DROP TRIGGER IF EXISTS trigger_update_ctgn;
+delimiter $$
+CREATE TRIGGER trigger_update_ctgn after update on ChiTietGomNhom for each row
+begin
+	if new.isDeleted = false then
+		call procedure_update_tcbb_gn_by_ctgn(new.ID_GomNhom);
+	end if;
+end
+$$
+delimiter ;
+
+DROP TRIGGER IF EXISTS trigger_insert_ctgn;
+delimiter $$
+CREATE TRIGGER trigger_insert_ctgn after insert on ChiTietGomNhom for each row
+begin
+	call procedure_update_tcbb_gn_by_ctgn(new.ID_GomNhom);
+end
+$$
+delimiter ;
+
+DROP procedure IF EXISTS procedure_update_tcbb_gn_by_ctgn;
+delimiter $$
+create procedure procedure_update_tcbb_gn_by_ctgn(in newIDGN int)
+begin
+	update GomNhom gn
+	inner join (
+		select ctgn.ID_GomNhom, sum(mh.soTinChi) as TongTC_GN 
+		from ChiTietGomNhom ctgn
+		inner join MonHoc mh ON mh.id = ctgn.ID_MonHoc
+		inner join GomNhom gn on gn.ID = ctgn.ID_GomNhom
+		group by ctgn.ID_GomNhom
+        having ctgn.ID_GomNhom = newIDGN and ctgn.isDeleted = false and mh.isDeleted = false and gn.isDeleted = false) nt1 
+	on gn.ID = nt1.ID_GomNhom 
+	set gn.SoTCBB = nt1.TongTC_GN
+    where gn.LoaiNhom = "BB" and gn.isDeleted = false;
+end
+$$
+delimiter ;
+
+
+# -------------------------------------------
+DROP TRIGGER IF EXISTS trigger_update_gn;
+delimiter $$
+CREATE TRIGGER trigger_update_gn after update on GomNhom for each row
+begin
+	if new.isDeleted = false then
+		call procedure_update_ttc_lkkt_by_gn(new.ID_LoaiKhoiKienThuc);
+	end if;
+end
+$$
+delimiter ;
+
+DROP TRIGGER IF EXISTS trigger_insert_gn;
+delimiter $$
+CREATE TRIGGER trigger_insert_gn after insert on GomNhom for each row
+begin
+	call procedure_update_ttc_lkkt_by_gn(new.ID_LoaiKhoiKienThuc);
+end
+$$
+delimiter ;
+
+DROP procedure IF EXISTS procedure_update_ttc_lkkt_by_gn;
+delimiter $$
+create procedure procedure_update_ttc_lkkt_by_gn(in newIdLKKT int)
+begin
+	update LoaiKhoiKienThuc lkkt
+	inner join (
+		select lkkt_temp.ID_KhoiKienThuc, sum(gn.SoTCBB) as TongTC
+		from LoaiKhoiKienThuc lkkt_temp
+		inner join GomNhom gn ON gn.ID_LoaiKhoiKienThuc = lkkt_temp.ID
+		group by lkkt_temp.ID
+        having lkkt_temp.ID = newIdLKKT and lkkt_temp.isDeleted = false and gn.isDeleted = false) nt1 
+	on lkkt.ID = nt1.ID_KhoiKienThuc 
+	set lkkt.TongTC = nt1.TongTC;
+end
+$$
+delimiter ;
---
+++ src/db/ratio_constraints_script.sql
@@ -0,0 +1,94 @@
+use `qHtJYlp09Y`;
+SET SQL_SAFE_UPDATES = 0;
+
+# -------------------------------------------
+DROP TRIGGER IF EXISTS trigger_update_hoat_dong_danh_gia;
+delimiter $$
+CREATE TRIGGER trigger_update_hoat_dong_danh_gia after update on HoatDongDanhGia for each row
+begin
+	if old.tyLe <> new.tyLe and new.isDeleted = false then
+		call procedure_update_ty_le_loai_danh_gia(new.idLDG);
+	end if;
+end
+$$
+delimiter ;
+
+DROP TRIGGER IF EXISTS trigger_insert_hoat_dong_danh_gia;
+delimiter $$
+CREATE TRIGGER trigger_insert_hoat_dong_danh_gia after insert on HoatDongDanhGia for each row
+begin
+	call procedure_update_ty_le_loai_danh_gia(new.idLDG);
+end
+$$
+delimiter ;
+
+DROP procedure IF EXISTS procedure_update_ty_le_loai_danh_gia;
+delimiter $$
+create procedure procedure_update_ty_le_loai_danh_gia(in newIDLDG int)
+begin
+	update LoaiDanhGia ldg
+	inner join (
+		select hddg.idLDG, sum(hddg.ty_le) as Tongty_le 
+		from HoatDongDanhGia hddg
+		group by hddg.idLDG
+        having hddg.idLDG = newIDLDG) nt1 
+	on ldg.ID = nt1.idLDG
+	set ldg.ty_le = nt1.Tongty_le;
+end
+$$
+delimiter ;
+
+# -------------------------------------------
+DROP TRIGGER IF EXISTS trigger_insert_loai_danh_gia;
+delimiter $$
+CREATE TRIGGER trigger_insert_loai_danh_gia before insert on LoaiDanhGia for each row
+begin
+	call procedure_check_ration_before_insert(new.idSyllabus, new.tyLe);
+end
+$$
+delimiter ;
+
+DROP TRIGGER IF EXISTS trigger_update_loai_danh_gia;
+delimiter $$
+CREATE TRIGGER trigger_update_loai_danh_gia before update on LoaiDanhGia for each row
+begin
+	if new.isDeleted = false then
+		call procedure_check_ration_before_update(new.id, new.idSyllabus, new.tyLe);
+	end if;
+end
+$$
+delimiter ;
+
+DROP procedure IF EXISTS procedure_check_ration_before_insert;
+delimiter $$
+create procedure procedure_check_ration_before_insert(in newIDSyllabus int, in newTyLe double)
+begin
+	declare s double;
+    select sum(ldg.tyLe) into s
+    from LoaiDanhGia ldg
+    where ldg.idSyllabus = newIDSyllabus and ldg.isDeleted = false;
+    
+    if(s + newTyLe > 1.0) then
+		signal sqlstate '45000'
+			set message_text = 'Sum of ration not greater than 100%';
+    end if;
+end
+$$
+delimiter ;
+
+DROP procedure IF EXISTS procedure_check_ration_before_update;
+delimiter $$
+create procedure procedure_check_ration_before_update(in newId int, in newIDSyllabus int, in newTyLe double)
+begin
+	declare s double;
+    select sum(ldg.tyLe) into s
+    from LoaiDanhGia ldg
+    where ldg.idSyllabus = newIDSyllabus and ldg.id != newId and ldg.isDeleted = false;
+    
+    if(s + newTyLe > 1.0) then
+		signal sqlstate '45000'
+			set message_text = 'Sum of ration not greater than 100%';
+    end if;
+end
+$$
+delimiter ;
