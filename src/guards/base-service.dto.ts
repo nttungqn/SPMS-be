2120a553d52bf1e3dd9a0eedf31e960dd9531f62
= <tranvi9x@gmail.com>
2021-04-21 12:12:06 +0700

    Fix bug

--- src/guards/base-service.dto.ts
+++ src/guards/base-service.dto.ts
@@ -2,7 +2,8 @@ import { ForbiddenException } from '@nestjs/common';
 import { ROLES_MESSAGE } from 'constant/constant';
 
 export class BaseService {
-  protected isOwner(createdBy: number, updatedBy: number) {
-    if (createdBy !== updatedBy) throw new ForbiddenException(ROLES_MESSAGE.NOT_OWNER);
+  protected isOwner(createdBy: any, updatedBy: number) {
+    const { id } = createdBy;
+    if (id !== updatedBy) throw new ForbiddenException(ROLES_MESSAGE.NOT_OWNER);
   }
 }
