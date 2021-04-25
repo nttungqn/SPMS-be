074f1117a9ed62909602991066de82cd26fb4e5c
= <tranvi9x@gmail.com>
2021-04-25 11:32:52 +0700

    Add file config

---
+++ src/firebase/.config.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAg9kS0xBK9GagQy98fX_nSzN9Eg9SjDL8',
+  authDomain: 'spms-f664f.firebaseapp.com',
+  projectId: 'spms-f664f',
+  storageBucket: 'spms-f664f.appspot.com',
+  messagingSenderId: '106291459378',
+  appId: '1:106291459378:web:159476d3c4fdf5e51ad32b',
+  measurementId: 'G-B0QRZVC2GM'
+};
