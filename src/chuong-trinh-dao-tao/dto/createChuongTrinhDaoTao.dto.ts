4d7fcfb74fe3b0f82014be5e7b3151e78589a33b
Phan Van Tuan <phanvantuan99hth@gmail.com>
2021-04-27 09:43:03 +0700

    add isnotemmpty decorator

--- src/chuong-trinh-dao-tao/dto/createChuongTrinhDaoTao.dto.ts
+++ src/chuong-trinh-dao-tao/dto/createChuongTrinhDaoTao.dto.ts
@@ -1,36 +1,44 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsString } from 'class-validator';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateChuongTrinhDaoTaoDto {
   @ApiProperty({ required: false })
   @IsString()
+  @IsNotEmpty()
   readonly maCTDT?: string;
 
   @IsString()
   @ApiProperty({ required: false })
+  @IsNotEmpty()
   readonly loaiHinh?: string;
 
   @IsString()
   @ApiProperty({ required: false })
+  @IsNotEmpty()
   readonly ten?: string;
 
   @IsString()
   @ApiProperty({ required: false })
+  @IsNotEmpty()
   readonly trinhDo?: string;
 
   @IsInt()
   @ApiProperty({ required: false, example: 0 })
+  @IsNotEmpty()
   readonly tongTinChi?: number;
 
   @IsString()
   @ApiProperty({ required: false })
+  @IsNotEmpty()
   readonly doiTuong?: string;
 
   @IsString()
   @ApiProperty({ required: false })
+  @IsNotEmpty()
   readonly quiTrinhDaoTao?: string;
 
   @IsString()
   @ApiProperty({ required: false })
+  @IsNotEmpty()
   readonly dieuKienTotNghiep?: string;
 }
