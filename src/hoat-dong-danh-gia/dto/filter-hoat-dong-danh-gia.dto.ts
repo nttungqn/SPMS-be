132fce4cf7e69b1affccbf5a31b2a09051997492
= <tranvi9x@gmail.com>
2021-04-29 23:58:31 +0700

    Sort for hoat-dong-danh-gia entity

--- src/hoat-dong-danh-gia/dto/filter-hoat-dong-danh-gia.dto.ts
+++ src/hoat-dong-danh-gia/dto/filter-hoat-dong-danh-gia.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { BaseFilterDto } from 'chuong-trinh-dao-tao/dto/filterChuongTrinhDaoTao.dto';
-import { IsNumberString, IsOptional } from 'class-validator';
+import { IsIn, IsNumberString, IsOptional } from 'class-validator';
 
 export class FilterHoatDongDanhGia extends BaseFilterDto {
   @ApiProperty({ required: false })
@@ -12,4 +12,21 @@ export class FilterHoatDongDanhGia extends BaseFilterDto {
   @IsOptional()
   @IsNumberString()
   idSyllabus: number;
+
+  @ApiProperty({ required: false, description: ' this field search by Ten ' })
+  @IsOptional()
+  readonly searchKey?: string;
+
+  @ApiProperty({
+    required: false,
+    description: 'Sort a string in column: loaiDanhGia, ten, ma, moTa, tyLe, updatedAt, createdAt, updatedBy, createdBy'
+  })
+  @IsOptional()
+  @IsIn(['loaiDanhGia', 'ten', 'ma', 'moTa', 'tyLe', 'updatedAt', 'createdAt', 'updatedBy', 'createdBy'])
+  sortBy: 'loaiDanhGia' | 'ma' | 'ten' | 'moTa' | 'tyLe' | 'updatedAt' | 'createdAt' | 'updatedBy' | 'createdBy';
+
+  @ApiProperty({ required: false, description: 'ASC| DESC' })
+  @IsOptional()
+  @IsIn(['ASC', 'DESC'])
+  sortType: 'ASC' | 'DESC';
 }
